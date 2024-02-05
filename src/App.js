7a0557aa6592fd6ccb1c0f00aa5097d95496c347
tushartyagi-086 <158012038+tushartyagi-086@users.noreply.github.com>
2024-02-05 22:03:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Prod from './Prod.jsx';
+
+function App() {
+  return (
+    <Prod />
+  )
+}
+
+export default App;
---
+++ src/Prod.jsx
@@ -0,0 +1,207 @@
+import React from 'react'
+import './Prod.css';
+
+const Prod = () => {
+  return (
+    <>
+    <body>
+    <nav>
+        <div>
+            <div id="left">
+                <a href="store.html">
+                    <img src="../pages/images/RBlogo.png" alt="Logo loading" />
+                 </a>
+            </div>
+            <div id="center">
+                <ul>
+                    <li><a class="active" href="products.html">Products</a></li>
+                    <li><a  href="about_us.html">About Us</a></li>
+                    <li><a href="../Home-Page/index.html">Bike Rental</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                </ul>
+            </div>
+            <div id="right">
+                <a href="x"><i class="fa fa-search" style={{fontSize:'48px',color:'black'}}></i></a>
+                <a href="home.html"><i class="fa fa-user-circle-o" style={{fontSize:'48px',color:'black'}}></i></a>
+                <a id="cart-icon" href="cart.html"><i class="fa fa-briefcase" style={{fontSize:'48px',color:'black'}}></i>
+                    
+                    <span id="cart-count">   </span>
+              </a> 
+            </div> 
+        </div>
+    </nav>
+
+  
+
+    <h1 class="center">Products</h1>
+    <div id="products">
+        <div id="contents">
+         <div class="p-box">
+            <img src="//cdn.shopify.com/s/files/1/0571/9906/7323/products/wht_345x345@2x.jpg?v=1637140841" alt="ab" />
+            <p>Adventure - Unisex T-Shirt </p>
+            <p> Rs. 499.00</p>
+         </div>
+         <div class="p-box">
+            <img src="//cdn.shopify.com/s/files/1/0571/9906/7323/products/Airman_Backrest_HimBS6_1_1024x1024_2x_357d642b-d8e6-4feb-840d-fa1f7b36a1c6_345x345@2x.jpg?v=1637586137" alt="a" />
+            <p>Airman Backrest for Royal Enfield Himalayan BS6 </p>
+            <p> Rs. 2,199.00</p>
+         </div>
+         <div class="p-box">
+            <img src="//cdn.shopify.com/s/files/1/0571/9906/7323/products/6-5_345x345@2x.jpg?v=1636440981" alt="q" />
+            <p>Alloy Headlight, Indicator Grill set (Royal Enfield </p>
+            <p> Rs. 1,799.00</p>
+         </div>
+         <div class="p-box">
+            <img src="//cdn.shopify.com/s/files/1/0571/9906/7323/products/CAMOUFLAGEJACKET-600x616_345x345@2x.png?v=1641635701" alt="t" />
+            <p>Axor Camouflage Riding Jacket </p>
+            <p> Rs. 6,245.00</p>
+         </div>
+         <div class="p-box">
+            <img src="//cdn.shopify.com/s/files/1/0571/9906/7323/products/9-1_345x345@2x.jpg?v=1636465637" alt="d" />
+            <p>Balaclava </p>
+            <p> Rs. 299.00</p>
+         </div>
+         <div class="p-box">
+            <img src="//cdn.shopify.com/s/files/1/0571/9906/7323/products/21-1_345x345@2x.jpg?v=1636467472" alt="q" />
+            <p>Bash Plate for Royal Enfield Classic 350 </p>
+            <p> Rs. 2,499.00</p>
+         </div>
+         <div class="p-box">
+            <img src="//cdn.shopify.com/s/files/1/0571/9906/7323/products/BBGBREEZEGLOVES-RED-1-600x616_345x345@2x.jpg?v=1641637434" alt="q" />
+            <p>BBG Breeze Black Red Riding Gloves </p>
+            <p> Rs. 2,500..00</p>
+         </div>
+         <div class="p-box">
+            <img src="//cdn.shopify.com/s/files/1/0571/9906/7323/products/BBGTAILBAG-600x616_345x345@2x.jpg?v=1642074232" alt="y" />
+            <p>BBG Hybrid Black Tail Bag</p>
+            <p> Rs. 3,500.00</p>
+         </div>
+         <div class="p-box">
+            <img src="//cdn.shopify.com/s/files/1/0571/9906/7323/products/BBG-SADDLEBAG-1-600x616_345x345@2x.jpg?v=1642074799" alt="s" />
+            <p>BBG Saddle Bag Set </p>
+            <p> Rs. 3,500.00</p>
+         </div>
+         <div class="p-box">
+            <img src="https://cdn.shopify.com/s/files/1/0571/9906/7323/products/BBG-Tank-Bag-1-600x616_345x345@2x.jpg?v=1642080182" alt="j" />
+            <p>BBG Tank Black Bag</p>
+            <p> Rs. 2,250.00</p>
+         </div>
+         <div class="p-box">
+            <img src="//cdn.shopify.com/s/files/1/0571/9906/7323/products/BBG-XPLORER-GY-01_345x345@2x.png?v=1631229366" alt="y" />
+            <p>BBG Xplorer Level 2 Riding Jacket - Black Grey </p>
+            <p> 6,750.00</p>
+         </div>
+         <div class="p-box">
+            <img src="//cdn.shopify.com/s/files/1/0571/9906/7323/products/1-2_345x345@2x.jpg?v=1636441996" alt="q" />
+            <p>Bike Antenna - 1 Ft</p>
+            <p> Rs. 550.00</p>
+         </div>
+         <div class="p-box">
+            <img src="//cdn.shopify.com/s/files/1/0571/9906/7323/products/Scribble-Pattern-1_345x345@2x.png?v=1623496545" alt="m" />
+            <p>Bike Scribble - Unisex T-Shirt</p>
+            <p> Rs. 3,250.00</p>
+         </div>
+         <div class="p-box">
+            <img src="//cdn.shopify.com/s/files/1/0571/9906/7323/products/0X0A1322-removebg-preview_345x345@2x.png?v=1654065648" alt="y" />
+            <p>BBG Xplorer Level 2 bungee cords </p>
+            <p>Rs. 750.00</p>
+         </div>
+         <div class="p-box">
+            <img src="//cdn.shopify.com/s/files/1/0571/9906/7323/products/27-1_345x345@2x.jpg?v=1636976535" alt="q" />
+            <p>Carrier for Bajaj Dominar</p>
+            <p> Rs. 1,999.00</p>
+         </div>
+    </div>
+
+      
+
+   <ul id="next-arrow-section" class="list--inline pagination">
+  
+    <li aria-hidden="true">
+      <button class="btn " disabled>
+       
+        <span class="icon__fallback-text">Previous page</span>
+      </button>
+    </li>
+    
+  <li class="pagination__text">
+    Page 1 of 5
+  </li>
+  
+    <li>
+      <a href="/products/page=2" class="btn">
+        
+        <span class="icon__fallback-text">Next page</span>
+      </a>
+    </li>
+  
+</ul>
+</div> 
+  
+
+ 
+
+ <footer id="home-Footer" >
+    <div id="main-footer" >
+         <div>
+             <h5>CarBorne</h5>
+             <hr />
+             <div>
+                 <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/white-mail-f7a800f328eab451231c558115ad0067bdd6c5ba04b7acd19f22ae4036e702d1.png" alt="phone icon" />
+             <p>support@CarBorne.com</p>
+             </div>
+             <div>
+                 <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/white-phone-d7daea12563d63fb4bd1cbb2fa0a824d19501def56934aff125e23343912895e.png" alt="phone icon" />
+                 <p>+919999999987/+919000980008</p>
+             </div> 
+         </div>
+         <div>
+             <h5>COMPANY</h5>
+             <hr />
+             <a href="w">About Us</a>
+             <a href="x">Blog</a>
+             <a href="y">Careers</a>
+             <a href="z">Contact Us</a>
+
+         </div>
+         <div>
+             <h5>POLICIES</h5>
+             <hr /> 
+             <p>Privacy Policy</p>
+             <p>Terms & Conditions</p>
+         </div>
+         <div>
+             <h5>QUICK LINKS</h5>
+             <hr />
+             <a href="x">Tariff</a>
+             <a href="y">Own a franchise</a>
+             <a href="r">Earn with us</a>
+             <a href="s">Indian bike routes</a>
+         </div>
+   
+         <div>
+             <h5>FOLLOW US</h5>
+             <hr />
+             <div class="socialmedia" >
+                 <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/Insta-e068316ffc568b3c0162ef129542c4ec473fedb2317cc8132168a07a0616b475.png" alt="insta logo" />
+                 <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/Facebook-ad24f92a1479ca4c5b308092f11f91d3ad2189d505ba2a737f376943a89b721b.png" alt="FB Logo" />
+                 <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/Linkedin-685bfa4464dc2263601740aad98014e02801299c7f2104be70458ac13d061fa1.png" alt="Linkedn Logo" />
+                 <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/Youtube-35dbc9202b8e7b71d09c782934215b9f6a78a1b3b11d190b5ad275c004484295.png" alt="ab" />
+             </div>    
+         </div>
+         <div class="followUs">
+             <h5>DOWNLOAD OUR APP</h5>
+             <hr />
+             <div>
+                 <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/app_store-4407bc8ced0d52a50ff042ce51e32d51de3e11717955798ec54c6a28e7926c7c.png" alt="App Store" />
+                 <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/play_store-057f9061b4cd0fd4953a485569c15e63fdf8d89b0bcc025c04cfb34d9e8cd571.png" alt="Play Store" />
+             </div>    
+         </div>     
+   </div> 
+</footer>
+</body>
+</>
+  )
+}
+
+export default Prod
